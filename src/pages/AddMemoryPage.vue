eaddc3bc83b959e97f429c378f2425c9d258bd0c
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-10-01 23:08:01 +0300

    added native camera support

--- src/pages/AddMemoryPage.vue
+++ src/pages/AddMemoryPage.vue
@@ -1,3 +1,14 @@
+<style lang="scss">
+.image-placeholder {
+   font-size: 3em;
+   display: grid;
+   place-self: center;
+   // justify-content: center;
+   height: 100%;
+   width: 100%;
+}
+</style>
+
 <template lang="pug">
 
 BaseLayout( title="add a memory" page-default-back-link="/memories" )
@@ -7,8 +18,12 @@ BaseLayout( title="add a memory" page-default-back-link="/memories" )
             IonLabel( position="floating" ) Title
             IonInput( required v-model="form.title" )
          IonItem
-            IonLabel( position="floating" ) Image URL
-            IonInput( type='url' required v-model="form.image" )
+            IonThumbnail( slot="start" )
+               img( :src="form.image" v-if="form.image != ''" )
+               IonIcon.image-placeholder( v-else :src="images" )
+            IonButton( fill="clear" @click="takePhoto" type="button" )
+               IonIcon( :icon="camIcon" slot="start" )
+               |Take Photo
          IonItem
             IonLabel( position="floating" ) Description 
             IonTextarea( rows="5" v-model="form.description" )
@@ -17,13 +32,28 @@ BaseLayout( title="add a memory" page-default-back-link="/memories" )
 </template>
 
 <script setup>
-import BaseLayout from '@/components/base/BaseLayout.vue';
-import { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton } from '@ionic/vue';
-import { reactive } from 'vue';
+import { IonList, IonItem, IonLabel, IonInput, IonTextarea, IonButton, IonThumbnail, IonIcon, IonActionSheet } from '@ionic/vue';
+import { reactive, ref } from 'vue';
 import { useRouter } from "vue-router";
-import { store } from '@/store'
+import { store } from '@/store';
+import { camera, images } from 'ionicons/icons';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera'
+
 
 const router = useRouter();
+
+const camIcon = ref(camera);
+
+const takePhoto = async () => {
+   const photo = await Camera.getPhoto({
+    quality: 90,
+    allowEditing: true,
+   resultType: CameraResultType.Uri,
+   source: CameraSource.Camera 
+   })
+
+   form.image = photo.webPath;
+}
 
 const form = reactive({
    title: '',
