ff1d5dd931e292e56e8acc8462a9e7cee9618d96
NyllRE <52743522+NyllRE@users.noreply.github.com>
2022-10-12 20:00:44 +0300

    added notification scaffold

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,12 +6,9 @@ const config: CapacitorConfig = {
   webDir: 'dist',
   bundledWebRuntime: false,
   plugins: {
-    LocalNotifications: {
-      smallIcon: "ic_stat_icon_config_sample",
-      iconColor: "#488AFF",
-      sound: "beep.wav",
+    PushNotifications: {
+      presentationOptions: ["badge", "sound", "alert"],
     },
-  },
-};
-
+  }
+}
 export default config;
--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ ion-app
 
 </template>
 <script>
+import { PushNotifications } from '@capacitor/push-notifications';
 import { IonApp, IonRouterOutlet } from '@ionic/vue';
 import { defineComponent } from 'vue';
 
@@ -33,4 +34,45 @@ onMounted(() => {
   
 
 })
+
+
+
+const addListeners = async () => {
+  await PushNotifications.addListener('registration', token => {
+    console.info('Registration token: ', token.value);
+  });
+
+  await PushNotifications.addListener('registrationError', err => {
+    console.error('Registration error: ', err.error);
+  });
+
+  await PushNotifications.addListener('pushNotificationReceived', notification => {
+    console.log('Push notification received: ', notification);
+  });
+
+  await PushNotifications.addListener('pushNotificationActionPerformed', notification => {
+    console.log('Push notification action performed', notification.actionId, notification.inputValue);
+  });
+}
+
+const registerNotifications = async () => {
+  let permStatus = await PushNotifications.checkPermissions();
+
+  if (permStatus.receive === 'prompt') {
+    permStatus = await PushNotifications.requestPermissions();
+  }
+
+  if (permStatus.receive !== 'granted') {
+    throw new Error('User denied permissions!');
+  }
+
+  await PushNotifications.register();
+}
+
+const getDeliveredNotifications = async () => {
+  const notificationList = await PushNotifications.getDeliveredNotifications();
+  alert('delivered notifications', ...notificationList)
+}
+
+getDeliveredNotifications()
 </script>
